cfa0473234bc03747f6aa53a890a9ee9befb9668
Ricardo Ferreira <ricardo199221@gmail.com>
2020-03-04 10:36:31 +0000

    Final Alfa Version

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 let gridResolution = 100;
 let grid = [];
+let status = false;
 
 const container = document.getElementById("container");
-const button = document.getElementById("button");
 let inputElement = document.getElementById("resInput");
+const button = document.getElementById("startStop");
 
 inputElement.addEventListener("change", function(e){
     gridResolution = e.target.value;
@@ -18,11 +19,23 @@ function createGrid(gridResolution){
     }
 }
 
-document.addEventListener("mouseover", function(e){
-    if(e.target && e.target.className=="cell-item"){
-        e.target.style.background = "black";
+
+
+button.addEventListener("click", function(){
+    if(status == 1){
+        status = false;
+        button.textContent = "Start"
+    }else{
+        status = true;
+        button.textContent = "Stop"
     }
-})
 
+    document.addEventListener("mouseover", function(e){
+         if(e.target && e.target.className=="cell-item"&&status == true){
+                e.target.style.background = "black";
+         }
+    })
+    
+   
+});
 createGrid(gridResolution);
-
